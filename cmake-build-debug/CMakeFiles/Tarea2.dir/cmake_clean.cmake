47e22475d1049191172aeba01f06ace817f30a5b
Abigail91 <abigail_abarca@hotmail.com>
2019-09-13 00:50:20 -0600

    Solución

---
+++ cmake-build-debug/CMakeFiles/Tarea2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tarea2.dir/main.cpp.o"
+  "Tarea2.pdb"
+  "Tarea2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tarea2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
